5388f6f47f9a1622bbc5ffc78737dbb971e3b27c
Felipe Thomaz Pedroni <felipe.pedroni@kiyosito.io>
2025-09-22 23:58:13 -0300

    Restore plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,39 +2,40 @@
 
 # name: discourse-siwe
 # about: A discourse plugin to enable users to authenticate via Sign In with Ethereum
-# version: 0.1.3
+# version: 0.1.2
 
 enabled_site_setting :discourse_siwe_enabled
 register_svg_icon 'fab-ethereum'
 register_asset 'stylesheets/discourse-siwe.scss'
 
-# Carrega a strategy
 %w[
   ../lib/omniauth/strategies/siwe.rb
 ].each { |path| load File.expand_path(path, __FILE__) }
 
-# Dependências SIWE
-gem 'pkg-config', '1.5.6'
-gem 'forwardable', '1.3.3'
-gem 'mkmfmf', '0.4'
-gem 'keccak', '1.3.0'
-gem 'zip', '2.0.2'
-gem 'mini_portile2', '2.8.0'
-gem 'rbsecp256k1', '6.0.0'
-gem 'konstructor', '1.0.2'
-gem 'ffi', '1.17.2'
-gem 'ffi-compiler', '1.0.1'
-gem 'scrypt', '3.0.7'
-gem 'eth', '0.5.11'
-gem 'siwe', '1.1.2'
+gem 'pkg-config', '1.5.6', require: false
+gem 'forwardable', '1.3.3', require: false
+gem 'mkmfmf', '0.4', require: false
+gem 'keccak', '1.3.0', require: false
+gem 'zip', '2.0.2', require: false
+gem 'mini_portile2', '2.8.0', require: false
+gem 'rbsecp256k1', '6.0.0', require: false
+gem 'konstructor', '1.0.2', require: false
+gem 'ffi', '1.17.2', require: false
+gem 'ffi-compiler', '1.0.1', require: false
+gem 'scrypt', '3.0.7', require: false
+gem 'eth', '0.5.11', require: false
+gem 'siwe', '1.1.2', require: false
 
 class ::SiweAuthenticator < ::Auth::ManagedAuthenticator
   def name
-    "siwe"
+    'siwe'
   end
 
   def register_middleware(omniauth)
-    omniauth.provider :siwe
+    omniauth.provider :siwe,
+                      setup: lambda { |env|
+                        strategy = env['omniauth.strategy']
+                      }
   end
 
   def enabled?
@@ -42,17 +43,15 @@ class ::SiweAuthenticator < ::Auth::ManagedAuthenticator
   end
 
   def primary_email_verified?
-    true
+    false
   end
 end
 
 auth_provider authenticator: ::SiweAuthenticator.new,
-              icon: "fab-ethereum",
-              full_screen_login: true,
-              title: "Sign in with Ethereum"
+              icon: 'fab-ethereum',
+              full_screen_login: true
 
 after_initialize do
-  # Se for expor rotas extras além do OmniAuth, mantém
   %w[
     ../lib/discourse_siwe/engine.rb
     ../lib/discourse_siwe/routes.rb
@@ -60,6 +59,6 @@ after_initialize do
   ].each { |path| load File.expand_path(path, __FILE__) }
 
   Discourse::Application.routes.prepend do
-    mount ::DiscourseSiwe::Engine, at: "/discourse-siwe"
+    mount ::DiscourseSiwe::Engine, at: '/discourse-siwe'
   end
 end
